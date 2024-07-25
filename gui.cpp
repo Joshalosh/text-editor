4db6d76a00311025d8d3768ace80ff39c656c24b
Joshalosh <josh.jessopsmith@gmail.com>
2024-07-26 09:15:39 +1000

    Start GUI version of editor

---
+++ gui.cpp
@@ -0,0 +1,56 @@
+#include <windows.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#define MAX_BUFFER_SIZE 1024
+#define MAX_LINES 100
+#define MAX_ROW_SIZE 80
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t  s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+#define Assert(expression) if(!(expression)) {*(int *)0 = 0;}
+
+u8 buffer[MAX_BUFFER_SIZE];
+
+struct File_State {
+    s32 cursor_index;
+    s32 buffer_index;
+    s32 buffer_count;
+    s32 row_count;
+    s32 row_line_sizes[MAX_LINES];
+};
+
+static File_State FileStateInit() {
+    File_State result = {};
+    result.cursor_index = 0;
+    result.buffer_index = 0;
+    result.buffer_count = 0;
+    result.row_count    = 0;
+
+    return result;
+}
+
+File_State file_state;
+
+s32 CursorXPosition(s32 cursor_index) {
+    return cursor_index % MAX_ROW_SIZE;
+}
+
+s32 CursorYPosition(s32 cursor_index) {
+    return cursor_index / MAX_ROW_SIZE;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void DrawText(HWND hwnd);
+void HandleCharInput(HWND hwnd, char c);
+void HandleBackspace(HWND hwnd); 
+void HandleArrowKeys(HWND hwnd, int key);
+
