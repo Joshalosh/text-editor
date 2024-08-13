6a1b459f666d5e5ce05d365219f1180186cd7976
Joshalosh <josh.jessopsmith@gmail.com>
2024-08-13 10:45:37 +1000

    Add more functions for GUI text editor

--- gui.cpp
+++ gui.cpp
@@ -149,3 +149,69 @@ void DrawText(HWND hwnd) {
     EndPaint(hwnd, &ps);
 }
 
+void HandleCharInput(HWND hwnd, char c) {
+    if (file_state.buffer_index < file_state.buffer_count) {
+        for (s32 i = file_state.buffer_count; i > file_state.buffer_index; i--) {
+            buffer[i] = buffer[i-1];
+        }
+    }
+
+    if (c == '\r') {
+        c = '\n';
+        s32 chars_for_new_line = file_state.row_line_sizes[CursorYPosition(file_state.cursor_index)] - 
+                                 CursorXPosition(file_state.cursor_index);
+        s32 next_line_start = (CursorYPosition(file_state.cursor_index) + 1) * MAX_ROW_SIZE;
+        if (next_line_start < MAX_ROW_SIZE) {
+            file_state.cursor_index = next_line_start;
+            file_state.row_count++;
+            if (CursorYPosition(file_state.cursor_index) < file_state.row_count) {
+                for (s32 i = file_state.row_count; i > CursorYPosition(file_state.cursor_index); i--) {
+                    file_state.row_line_sizes[i] = file_state.row_line_sizes[i-1];
+                }
+            }
+            file_state.row_line_sizes[CursorYPosition(file_state.cursor_index)] = 0;
+            file_state.row_line_sizes[CursorYPosition(file_state.cursor_index)] += chars_for_new_line;
+            file_state.row_line_sizes[CursorYPosition(file_state.cursor_index)-1] -= chars_for_new_line;
+        } else {
+            file_state.row_line_sizes[CursorYPosition(file_state.cursor_index)] += 1;
+            file_state.cursor_index++;
+        }
+        buffer[file_state.buffer_index++] = c;
+        file_state.buffer_count++;
+        InvalidateRect(hwnd, NULL, true);
+    }
+}
+
+void HandleBackspace(HWND hwnd) {
+    if (file_state.buffer_index > 0) {
+        for (s32 i = file_state.buffer_index - 1; i < file_state.buffer_count; i++) {
+            buffer[i] = buffer[i+1];
+        }
+
+        if (CursorXPosition(file_state.cursor_index) == 0) {
+            s32 previous_row_size = file_state.row_line_sizes[CursorYPosition(file_state.cursor_index)] + 1;
+            s32 previous_line = CursorYPosition(file_state.cursor_index) - 1;
+            s32 null_space = MAX_ROW_SIZE file_state.row_line_sizes[previous_line];
+            file_state.cursor_index -= null_space;
+            file_state.row_line_sizes[CursorYPosition(file_state.cursor_index)] += previous_row_size;
+
+            if (CursorYPosition(file_state.cursor_index) < file_state.row_count) {
+                for (s32 i = CursorYPosition(file_state.cursor_index) + 1; i < file_state.row_count; i++) {
+                    file_state.row_line_sizes[i] = file_state.row_line_sizes[i+1];
+                }
+                file_state.row_count--;
+            }
+        } else {
+            file_state.cursor_index--;
+        }
+
+        file_state.row_line_sizes[CursorYPosition(file_state.cursor_index)] -= 1;
+        file_state.buffer_count--;
+        file_state.buffer_index--;
+        InvalidateRect(hwnd, NULL, true);
+    }
+}
+
+void HandleArrowKeys(HWND hwnd, int key) {
+}
+
